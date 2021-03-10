34fb4ddfa665cfd9ebc3f635b04391bebc29c8ad
AlbinKryeziu <57589969+AlbinKryeziu@users.noreply.github.com>
2021-03-10 14:47:57 +0100

    view

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('jen/pages/new-home');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
