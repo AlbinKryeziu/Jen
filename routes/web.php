698f84595b4ec1a21062fff2f45f25d51a62a901
AlbinKryeziu <57589969+AlbinKryeziu@users.noreply.github.com>
2021-03-11 22:56:44 +0100

    login

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+   return view('jen/pages/new-home');
 })->name('dashboard');
 
 Route::post('doctor/account', [RegisterController::class,'registerDoctor']);
