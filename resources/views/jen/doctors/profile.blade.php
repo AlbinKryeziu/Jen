5e565cd0a76030fbbc2ae52d414adfa5ac4bf632
AlbinKryeziu <57589969+AlbinKryeziu@users.noreply.github.com>
2021-04-19 17:00:13 +0200

    fixes

--- resources/views/jen/doctors/profile.blade.php
+++ resources/views/jen/doctors/profile.blade.php
@@ -60,7 +60,7 @@
     @import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
 </style>
 
-<section id="hero" class="d-flex align-items-center" style="background: url('{{ asset('jen/assets/img/doctor.jpg') }}') top center; background-size: cover ">
+<section id="hero" class="d-flex align-items-center" style="background: url('{{ asset('jen/assets/img/img1.jpeg') }}') top center; background-size: cover ">
     <div class="container"></div>
 </section>
 
@@ -253,7 +253,7 @@
         <div class="container d-md-flex py-4">
             <div class="mr-md-auto text-center text-md-left">
                 <div class="copyright">
-                    &copy; Copyright <strong><span>Jen</span></strong>. All Rights Reserved
+                    &copy; Copyright <strong><span>SelfPayNet</span></strong>. All Rights Reserved
                 </div>
                 <div class="credits"></div>
             </div>
--- resources/views/jen/pages/new-home.blade.php
+++ resources/views/jen/pages/new-home.blade.php
@@ -818,7 +818,7 @@ section .section-title {
 
       <div class="mr-md-auto text-center text-md-left">
         <div class="copyright">
-          &copy; Copyright <strong><span>Jen</span></strong>. All Rights Reserved
+          &copy; Copyright <strong><span>SelfPayNet</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
             
